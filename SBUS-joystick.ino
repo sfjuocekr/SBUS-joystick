57db638361e209b4c37d48b85973f7cfcc54378a
Sjoer van der Ploeg <sfjuocekr@gmail.com>
2018-08-05 16:48:37 +0200

    Cleanup

--- SBUS-joystick.ino
+++ SBUS-joystick.ino
@@ -7,6 +7,10 @@
  * Sjoer van der Ploeg, 2018
 **/
 
+// SBUS range
+#define STARTPOINT 240
+#define ENDPOINT 1807
+
 // Channel mapping
 #define PITCH 1
 #define ROLL 2
@@ -24,8 +28,7 @@
 SBUS sbus(Serial1);
 
 uint16_t channels[16];
-bool failSafe;
-bool lostFrame;
+bool failSafe, lostFrame;
 
 void setup()
 {
@@ -37,27 +40,27 @@ void loop()
 {
   if(sbus.read(&channels[0], &failSafe, &lostFrame))
   {
-      Joystick.X(map(channels[ROLL - 1], 240, 1807, 0, 65535));
-      Joystick.Y(map(channels[PITCH - 1], 240, 1807, 0, 65535));      
-      Joystick.Z(map(channels[THROTTLE - 1], 240, 1807, 0, 65535));
+      Joystick.X(map(channels[ROLL - 1], STARTPOINT, ENDPOINT, 0, 65535));
+      Joystick.Y(map(channels[PITCH - 1], STARTPOINT, ENDPOINT, 0, 65535));      
+      Joystick.Z(map(channels[THROTTLE - 1], STARTPOINT, ENDPOINT, 0, 65535));
       
-      Joystick.Zrotate(map(channels[YAW - 1], 240, 1807, 0, 65535));
-      Joystick.Xrotate(map(channels[AUX1 - 1], 240, 1807, 0, 65535));
-      Joystick.Yrotate(map(channels[AUX2 - 1], 240, 1807, 0, 65535));
+      Joystick.Zrotate(map(channels[YAW - 1], STARTPOINT, ENDPOINT, 0, 65535));
+      Joystick.Xrotate(map(channels[AUX1 - 1], STARTPOINT, ENDPOINT, 0, 65535));
+      Joystick.Yrotate(map(channels[AUX2 - 1], STARTPOINT, ENDPOINT, 0, 65535));
       
-      Joystick.button(1, map(channels[SWA - 1], 240, 1807, 0, 2) == 0 ? 1 : 0); // SWA LOW = button 1
-      Joystick.button(2, map(channels[SWA - 1], 240, 1807, 0, 2) == 1 ? 1 : 0); // SWA MID = button 2
-      Joystick.button(3, map(channels[SWA - 1], 240, 1807, 0, 2) == 2 ? 1 : 0); // SWA HIGH = button 3
+      Joystick.button(1, map(channels[SWA - 1], STARTPOINT, ENDPOINT, 0, 2) == 0 ? 1 : 0); // SWA LOW = button 1
+      Joystick.button(2, map(channels[SWA - 1], STARTPOINT, ENDPOINT, 0, 2) == 1 ? 1 : 0); // SWA MID = button 2
+      Joystick.button(3, map(channels[SWA - 1], STARTPOINT, ENDPOINT, 0, 2) == 2 ? 1 : 0); // SWA HIGH = button 3
       
-      Joystick.button(4, map(channels[SWB - 1], 240, 1807, 0, 2) == 0 ? 1 : 0); // SWB LOW = button 4
-      Joystick.button(5, map(channels[SWB - 1], 240, 1807, 0, 2) == 1 ? 1 : 0); // SWB MID = button 5
-      Joystick.button(6, map(channels[SWB - 1], 240, 1807, 0, 2) == 2 ? 1 : 0); // SWB HIGH = button 6
+      Joystick.button(4, map(channels[SWB - 1], STARTPOINT, ENDPOINT, 0, 2) == 0 ? 1 : 0); // SWB LOW = button 4
+      Joystick.button(5, map(channels[SWB - 1], STARTPOINT, ENDPOINT, 0, 2) == 1 ? 1 : 0); // SWB MID = button 5
+      Joystick.button(6, map(channels[SWB - 1], STARTPOINT, ENDPOINT, 0, 2) == 2 ? 1 : 0); // SWB HIGH = button 6
       
-      Joystick.button(7, map(channels[SWC - 1], 240, 1807, 0, 1) == 0 ? 1 : 0); // SWC LOW = button 7
-      Joystick.button(8, map(channels[SWC - 1], 240, 1807, 0, 1) == 1 ? 1 : 0); // SWC HIGH = button 8
+      Joystick.button(7, map(channels[SWC - 1], STARTPOINT, ENDPOINT, 0, 1) == 0 ? 1 : 0); // SWC LOW = button 7
+      Joystick.button(8, map(channels[SWC - 1], STARTPOINT, ENDPOINT, 0, 1) == 1 ? 1 : 0); // SWC HIGH = button 8
       
-      Joystick.button(9, map(channels[SWD - 1], 240, 1807, 0, 1) == 0 ? 1 : 0); // SWD LOW = button 9
-      Joystick.button(10, map(channels[SWD - 1], 240, 1807, 0, 1) == 1 ? 1 : 0); // SWD HIGH = button 10
+      Joystick.button(9, map(channels[SWD - 1], STARTPOINT, ENDPOINT, 0, 1) == 0 ? 1 : 0); // SWD LOW = button 9
+      Joystick.button(10, map(channels[SWD - 1], STARTPOINT, ENDPOINT, 0, 1) == 1 ? 1 : 0); // SWD HIGH = button 10
       
       Joystick.send_now();
   }
